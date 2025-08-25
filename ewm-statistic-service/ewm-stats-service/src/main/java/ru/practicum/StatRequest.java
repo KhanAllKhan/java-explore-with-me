4d9876970faa34dbbe2d60e7c587f12c1f559371
Khs <saha20142@yandex.kz>
2025-08-25 20:56:14 +0500

    Димпломный проект попытка 2

--- ewm-statistic-service/ewm-stats-service/src/main/java/ru/practicum/StatRequest.java
+++ ewm-statistic-service/ewm-stats-service/src/main/java/ru/practicum/StatRequest.java
@@ -3,8 +3,8 @@ package ru.practicum;
 import lombok.Data;
 
 import java.time.LocalDateTime;
-@Data
 
+@Data
 public class StatRequest {
     private String endpoint;
     private LocalDateTime startDate;
